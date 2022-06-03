066c4e4d660d0c5c874f00c65b3708a9ac9edd0d
Md Abdul Alim <abdulalimdhuronto24@gmail.com>
2022-06-03 16:47:38 +0600

    update

--- Structured_Programming_lab/lab_1/exercise_4.c
+++ Structured_Programming_lab/lab_1/exercise_4.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
+
 int main()
 {
-    int N;
+    double F,f_to_c;
 
     printf("Please input Fahrenheit : ");
-    scanf("%d",&N);
+    scanf("%lf",&F);
 
-    
+    f_to_c = (F-32)/9 * 5;
+
+    printf("%.2lf",f_to_c);
     
     return 0;
 }
