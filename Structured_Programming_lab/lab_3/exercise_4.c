89ae388aa6ee83c7cade85d0e9649dea5c079e4b
Md Abdul Alim <abdulalimdhuronto24@gmail.com>
2022-06-06 14:45:45 +0600

    update

--- Structured_Programming_lab/lab_3/exercise_4.c
+++ Structured_Programming_lab/lab_3/exercise_4.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+
+
+float fact(int N){
+
+    if(N == 1){
+        return 1;
+    }
+    else{
+        return N*fact(N-1);
+    }
+}
+
+
 int main()
 {
+    int N;
+    float sum = 0.0;
 
-return 0;
+    printf("Please input number of terms :\n");
+    scanf("%d",&N);
+
+    for(int i=1; i<= N; i++){
+        sum = sum + ( i/fact(i) );
+    }
+
+    printf("%d terms of series is : %.2f",N,sum);
+
+    
+
+    return 0;
 }
