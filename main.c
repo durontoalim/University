22192ecc617d92cfcfc723e86202d2fa21e5153d
Md Abdul Alim <abdulalimdhuronto24@gmail.com>
2022-08-17 20:23:34 +0600

    update

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char ch[50] = "Bangladesh";
+
+
+    printf("again(h\'yes\')\n");
+
+    printf("%20s\n",ch);
+    printf("%2s\n",ch);
+    printf("%20.0s\n",ch);
+    printf("%.5s\n",ch);
+
+    return 0;
+}
